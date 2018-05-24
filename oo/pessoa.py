93fe9b24379a48f29167a288a92cd26f61060234
Antonio Thomazinho <apthomazinho@gmail.com>
2018-05-23 22:09:05 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, nome = None, idade=35):
+        #Atributo antes do = Após a virgula temos um parametro ou uma variável
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Thomazinho')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Antonio'
+    print(p.nome)
+    print(p.idade)
